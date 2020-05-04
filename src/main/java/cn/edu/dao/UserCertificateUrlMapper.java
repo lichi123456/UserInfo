7fb7d1e903a22e8b5787b9f87ef92d465457f208
李翅 <3338763837@qq.com>
2020-05-04 17:13:34 +0800

    李某人，提交usercertificateUrlMapper

---
+++ src/main/java/cn/edu/dao/UserCertificateUrlMapper.java
@@ -0,0 +1,7 @@
+package cn.edu.dao;
+
+import cn.edu.vo.UserCertificateUrl;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserCertificateUrlMapper extends Mapper<UserCertificateUrl> {
+}
